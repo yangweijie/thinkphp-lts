4b5fc4fb966229e77ce686fc8444825c1d49eddc
yangweijie <917647288@qq.com>
2018-02-03 13:36:50 +0800

    视图原因将其默认写死在模板里

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -18,9 +18,11 @@ use think\facade\Env;
 class Index extends Controller
 {
 	public function index(){
+		$info = '<p>本速查表里的类都是think为命名空间的，实例化时省去了 &nbsp;use。用的时候注意。</p><p>本速查表里会有四种方法的调用：</p><p>&nbsp; &nbsp; 公有方法 $class = new Class(); &nbsp;$class-&gt;foo();<br></p><p>&nbsp; &nbsp; 公有静态 Class::foo();<br></p><p>&nbsp; &nbsp; 私有方法 $this-&gt;foo();<br></p><p>&nbsp; &nbsp; 私有静态 self::foo();<br></p><p>关于注释，为了简洁，/** 的单行注释被我改为了 //&nbsp;</p><p><br></p>';
 		$info = [
 			'name'        => sprintf('ThinkPHP %s 速查表', App::VERSION),
-			'info'        => '<p>本速查表里的类都是think为命名空间的，实例化时省去了 &nbsp;use。用的时候注意。</p><p>本速查表里会有四种方法的调用：</p><p>&nbsp; &nbsp; 公有方法 $class = new Class(); &nbsp;$class-&gt;foo();<br></p><p>&nbsp; &nbsp; 公有静态 Class::foo();<br></p><p>&nbsp; &nbsp; 私有方法 $this-&gt;foo();<br></p><p>&nbsp; &nbsp; 私有静态 self::foo();<br></p><p>关于注释，为了简洁，/** 的单行注释被我改为了 //&nbsp;</p><p><br></p>',
+			// 'info'        => base64_encode($info),
+			'info'=>$info,
 			'description' => 'ThinkPHP Cheat Sheet , Codes , function , methods of laravel framework',
 			'author'      => 'yangweijie',
 		];
